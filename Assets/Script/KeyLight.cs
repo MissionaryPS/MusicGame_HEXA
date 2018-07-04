e0f0e03c197014f52ce5ea9d5a9a152835e5ebf6
MissionaryPS <39906446+MissionaryPS@users.noreply.github.com>
2018-07-04 19:41:06 +0900

    functionize and coroutine
    関数とコルーチンの導入に成功
    外山君にお礼をせねば。

--- Assets/Script/KeyLight.cs
+++ Assets/Script/KeyLight.cs
@@ -4,43 +4,33 @@ using UnityEngine;
 
 public class KeyLight : MonoBehaviour {
 
+    Settings set;
     public GameObject[]key = new GameObject[6];
     public Renderer[] rend = new Renderer[6];
     public Color EffColor = new Color(1.0f, 0.5f, 1.0f);
     public Color DefColor = new Color(1.0f, 1.0f, 1.0f);
-    public string[] ObjName = new string[6] { "Key0", "Key1", "Key2", "Key3", "Key4", "Key5" };
-    public KeyCode[] KeyConfig = new KeyCode[6] { KeyCode.V, KeyCode.D, KeyCode.R, KeyCode.U, KeyCode.K, KeyCode.N };
 
     // Use this for initialization
     void Start () {
+        set = GameObject.Find("ScriptManager").GetComponent<Settings>();
         int i; 
         for(i = 0; i < 6; i++) {
             Debug.Log(i);
-            key[i] = GameObject.Find(ObjName[i]);
-            Debug.Log("key is found");
-            rend[i] = key[i].GetComponent<Renderer>();
+            rend[i] = GameObject.Find(set.ObjName[i]).GetComponent<Renderer>();
             Debug.Log("renderer is gotten");
             rend[i].material.EnableKeyword("_EMISSION");
             Debug.Log("emissiona enabled");
         }
 
     }
-    
-    void Update()
-    {
-        int i;
-        for (i = 0; i < 6; i++)
-        {
-            if (Input.GetKeyDown(KeyConfig[i]))
-            {
-                Debug.Log(i);
-                rend[i].material.SetColor("_EmissionColor", EffColor);
 
-            }
-            if (Input.GetKeyUp(KeyConfig[i]))
-            {
-                rend[i].material.SetColor("_EmissionColor", DefColor);
-            }
-        }
+    public void turnOn(int i) {
+        rend[i].material.SetColor("_EmissionColor", EffColor);
     }
+
+    public void turnOff(int i)
+    {
+        rend[i].material.SetColor("_EmissionColor", DefColor);
+    }
+
 }
---
+++ Assets/Script/playMain.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playMain : MonoBehaviour {
+
+    Settings set;
+    KeyLight keyEffect;
+    bool[] temp = new bool[6];
+
+    // Use this for initialization
+	void Start () {
+        int i;
+        for (i = 0; i < 6; i++) temp[i] = false;
+        set = GameObject.Find("ScriptManager").GetComponent<Settings>();
+        keyEffect = GameObject.Find("ScriptManager").GetComponent<KeyLight>();
+        StartCoroutine("playMusicGame");
+
+    }
+
+    IEnumerator playMusicGame()
+    {
+        Debug.Log("StartCoroutine");
+        while (true)
+        {
+            
+            int i;
+            for (i = 0; i < 6; i++)
+            {
+                if (Input.GetKey(set.KeyConfig[i]) != temp[i])
+                {
+                    Debug.Log(i);
+                    if (temp[i])
+                    {
+                        keyEffect.turnOff(i);
+                        temp[i] = false;
+                    }
+                    else
+                    { 
+                        keyEffect.turnOn(i);
+                        temp[i] = true;
+                    }
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Debug.Log("Coroutine end.");
+                yield break;
+            }
+            yield return new WaitForSeconds(0.03f);
+        }
+    }   
+
+
+}
