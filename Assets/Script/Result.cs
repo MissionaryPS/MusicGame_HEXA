f2dec30c1e7417b69800cac560b2cb9a31d665b6
MissionaryPS <39906446+MissionaryPS@users.noreply.github.com>
2018-07-19 09:28:32 +0900

    Resultクラスの実装、その他
    リザルト画面で必要そうな変数をResultクラスに入れておいた
    wavファイル一時削除

---
+++ Assets/Script/Result.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Result : MonoBehaviour {
+    public int score; //パラメータ実装予定
+    //判定関係
+    public int perfect;
+    public int great;
+    public int miss;
+    public double accuracy; //パラメータ実装予定
+    //コンボ関係
+    public int combo;
+    public int maxCombo; //パラメータ実装予定
+    public int sumCombo;
+    public double comboRate; //パラメータ実装予定
+
+
+
+    void Start()
+    {
+
+    }
+}
