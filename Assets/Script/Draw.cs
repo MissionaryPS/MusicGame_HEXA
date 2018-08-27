9e04ad8480ef9009ed5ac9b5ef9f85c826817496
MissionaryPS <39906446+MissionaryPS@users.noreply.github.com>
2018-08-28 06:08:46 +0900

    六角形のメッシュ化
    バグあり
    表示できない

--- Assets/Script/Draw.cs
+++ Assets/Script/Draw.cs
@@ -4,13 +4,31 @@ using UnityEngine;
 
 public class Draw : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    [SerializeField]
+    private MeshFilter meshFilter;
+    private Mesh mesh;
+    private List<Vector3> Hex = new List<Vector3>();
+    private List<int> indexHex = new List<int>();
+
+
+    private Mesh CreateKeys() {
+        var mesh = new Mesh();
+        //頂点設定
+        Hex.Add(new Vector3(0, 0));
+        for (int i = 0; i< 5; i++) Hex.Add(new Vector3(5 * Mathf.Cos(i* 60), 5 * Mathf.Sin(i* 60)));
+        indexHex.AddRange(new[] { 0, 1, 2, 0, 2, 3, 0, 3, 4, 0, 4, 5, 0, 5, 6, 0, 6, 1 });
+
+        mesh.SetVertices(Hex);
+        mesh.SetIndices(indexHex.ToArray(), MeshTopology.Triangles, 0);
+        return mesh;
+    }
+
+// Use this for initialization
+    void Start () {
+        mesh = CreateKeys();
+        meshFilter.mesh = mesh;
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
 }
