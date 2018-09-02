1ac4e019aed4ef06f844f527de37c4e318a3e04f
MissionaryPS <39906446+MissionaryPS@users.noreply.github.com>
2018-09-02 22:09:21 +0900

    六角形メッシュ、バグ直らず
    エラー内容としては、verticesとindexの数がおかしいらしい

--- Assets/Script/Draw.cs
+++ Assets/Script/Draw.cs
@@ -2,24 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshFilter))]
+
 public class Draw : MonoBehaviour {
 
     [SerializeField]
     private MeshFilter meshFilter;
+
+    [SerializeField]
+    private const float radius = 5f;
+
+    [SerializeField]
+    private const int vertices = 6;
+
     private Mesh mesh;
+
     private List<Vector3> Hex = new List<Vector3>();
     private List<int> indexHex = new List<int>();
 
 
     private Mesh CreateKeys() {
-        var mesh = new Mesh();
         //頂点設定
-        Hex.Add(new Vector3(0, 0));
-        for (int i = 0; i< 5; i++) Hex.Add(new Vector3(5 * Mathf.Cos(i* 60), 5 * Mathf.Sin(i* 60)));
-        indexHex.AddRange(new[] { 0, 1, 2, 0, 2, 3, 0, 3, 4, 0, 4, 5, 0, 5, 6, 0, 6, 1 });
+        Hex.Add(Vector3.zero);
+        for (int i = 0; i < vertices; i++)
+        {
+            float rad = (90f - (360f / (float)vertices) * i * Mathf.Deg2Rad);
+            float x = radius * Mathf.Cos(rad);
+            float y = radius * Mathf.Sin(rad);
+            Hex.Add(new Vector3(x, y, 0));
+            indexHex.Add(0);
+            indexHex.Add(i + 1);
+            indexHex.Add(i == vertices ? 1 : i + 2);
+            
+        }
 
-        mesh.SetVertices(Hex);
-        mesh.SetIndices(indexHex.ToArray(), MeshTopology.Triangles, 0);
+        var mesh = new Mesh();
+        mesh.vertices = Hex.ToArray();
+        mesh.triangles = indexHex.ToArray();
         return mesh;
     }
 
@@ -28,6 +48,8 @@ public class Draw : MonoBehaviour {
         mesh = CreateKeys();
         meshFilter.mesh = mesh;
 
+        var filter = GetComponent<MeshFilter>();
+        filter.sharedMesh = mesh;
 	}
 	
 	// Update is called once per frame
