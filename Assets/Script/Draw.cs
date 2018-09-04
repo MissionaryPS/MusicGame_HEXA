f62d948d9846a3e29fda7f0020a14b00968fa1e7
MissionaryPS <39906446+MissionaryPS@users.noreply.github.com>
2018-09-05 05:47:40 +0900

    サンプルコードコピペ改変
    六角形を描くものから三角形6つを描画する目的で改変したつもりだが、三角形は一度描画された後次の三角形に再描画され、もとの三角形が消える。増やすべきはMeshFilterではなくGameObject説が濃厚？

--- Assets/Script/Draw.cs
+++ Assets/Script/Draw.cs
@@ -8,49 +8,57 @@ using UnityEngine;
 public class Draw : MonoBehaviour {
 
     [SerializeField]
-    private MeshFilter meshFilter;
+    private Material _mat;
 
+    //頂点数
     [SerializeField]
-    private const float radius = 5f;
+    private int VerticesCount = 6;
 
+    //半径
     [SerializeField]
-    private const int vertices = 6;
+    private float Radius = 5f;
 
-    private Mesh mesh;
+    private List<MeshFilter> Keys = new List<MeshFilter>();
 
-    private List<Vector3> Hex = new List<Vector3>();
-    private List<int> indexHex = new List<int>();
-
-
-    private Mesh CreateKeys() {
-        //頂点設定
-        Hex.Add(Vector3.zero);
-        for (int i = 0; i < vertices; i++)
+    private void Start()
+    {
+        if (VerticesCount < 3)
         {
-            float rad = (90f - (360f / (float)vertices) * i * Mathf.Deg2Rad);
-            float x = radius * Mathf.Cos(rad);
-            float y = radius * Mathf.Sin(rad);
-            Hex.Add(new Vector3(x, y, 0));
-            indexHex.Add(0);
-            indexHex.Add(i + 1);
-            indexHex.Add(i == vertices ? 1 : i + 2);
-            
+            Debug.LogError("頂点数は３以上を指定してください。");
+            return;
         }
 
-        var mesh = new Mesh();
-        mesh.vertices = Hex.ToArray();
-        mesh.triangles = indexHex.ToArray();
-        return mesh;
+        List<Vector3> vertices = new List<Vector3>();
+        List<List<int>> triangles = new List<List<int>>();
+
+        //原点座標
+        vertices.Add(Vector3.zero);
+
+        //各頂点座標
+        for (int i = 1; i <= this.VerticesCount; i++)
+        {
+            float rad = (90f - (360f / (float)this.VerticesCount) * (i - 1)) * Mathf.Deg2Rad;
+            float x = Radius * Mathf.Cos(rad);
+            float y = Radius * Mathf.Sin(rad);
+            vertices.Add(new Vector3(x, y, 0));
+            var tri = new List<int>();
+            tri.Add(0);
+            tri.Add(i);
+            tri.Add(i == this.VerticesCount ? 1 : i + 1);
+            triangles.Add(tri);
+        }
+    
+
+        for (int i = 0; i < 4; i++) {
+            var mesh = new Mesh();
+            mesh.vertices = vertices.ToArray();
+            mesh.triangles = triangles[i].ToArray();
+            var filter = GetComponent<MeshFilter>();
+            Keys.Add(filter);
+            Keys[i].sharedMesh = mesh;
+        }
+
+        var renderer = GetComponent<MeshRenderer>();
+        renderer.material = _mat;
     }
-
-// Use this for initialization
-    void Start () {
-        mesh = CreateKeys();
-        meshFilter.mesh = mesh;
-
-        var filter = GetComponent<MeshFilter>();
-        filter.sharedMesh = mesh;
-	}
-	
-	// Update is called once per frame
 }
--- Assets/Script/KeyLight.cs
+++ Assets/Script/KeyLight.cs
@@ -18,11 +18,10 @@ public class KeyLight : MonoBehaviour {
             keyColor[i] = GameObject.Find(set.ObjName[i]).GetComponent<Renderer>();
             keyColor[i].material.EnableKeyword("_EMISSION");
             Debug.Log("emission enabled");
-            DefColor = new Color(1.0f, 1.0f, 1.0f);
-            EffColor[0] = new Color(1.0f, 0.5f, 1.0f);
-            EffColor[1] = new Color(1.0f, 1.0f, 0.1f);
         }
-
+        DefColor = new Color(1.0f, 1.0f, 1.0f);
+        EffColor[0] = new Color(1.0f, 0.5f, 1.0f);
+        EffColor[1] = new Color(1.0f, 1.0f, 0.1f);
     }
 
     public void TurnOn(int i, int color) {
