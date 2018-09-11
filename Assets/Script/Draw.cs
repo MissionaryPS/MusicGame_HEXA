64888cd1aaccc4c28ffddcd7f44997eccd36fd6e
MissionaryPS <39906446+MissionaryPS@users.noreply.github.com>
2018-09-11 14:24:56 +0900

    判定ライン、レーンおよび穴のメッシュ化
    その他いくつかの変数名の修正

--- Assets/Script/Draw.cs
+++ Assets/Script/Draw.cs
@@ -11,20 +11,22 @@ public class Draw : playMain {
     private GameObject KeyBase; //鍵のprehub
 
     [SerializeField]
-    private int VerticesCount = 6;    //頂点数
+    private GameObject JudgeLinePrehub;
 
     [SerializeField]
-    private float Radius = 5f;    //半径
+    private GameObject LaneLinePrehub;
+
+    [SerializeField]
+    private int VerticesCount = 6;    //頂点数
+
+    private const float KeyRadius = 5f;    //半径
+    private const float HoleRadius = 1.5f;    //半径
+
 
     private GameObject[] Key = new GameObject[6];
     private MeshRenderer[] KeyColor = new MeshRenderer[6];
     private Color[] EffColor = new Color[2];
     private Color DefColor;
-
-
-
-
-
 
     public void TurnOn(int i, int color)
     {
@@ -48,10 +50,14 @@ public class Draw : playMain {
         for (int i = 0; i < VerticesCount; i++)
         {
             Key[i] = Instantiate(KeyBase) as GameObject;
+            Key[i].name = "Key" + i;
             Key[i].GetComponent<MeshFilter>().sharedMesh = SetupKeyMesh(i);
             KeyColor[i] = Key[i].GetComponent<MeshRenderer>();
             KeyColor[i].material.EnableKeyword("_EMISSION");
         }
+
+        CreateJudgeLine();
+        CreateLaneLine();
         DefColor = new Color(1.0f, 1.0f, 1.0f);
         EffColor[0] = new Color(1.0f, 0.5f, 1.0f);
         EffColor[1] = new Color(1.0f, 1.0f, 0.1f);
@@ -62,20 +68,24 @@ public class Draw : playMain {
         List<Vector3> vertices = new List<Vector3>();
         List<int> triangles = new List<int>();
 
-        //原点座標
-        vertices.Add(Vector3.zero);
         //各頂点座標
         for (int j = i; j <= i + 1; j++) { 
-            float rad = (210f - (360f / (float)this.VerticesCount) * (j - 1)) * Mathf.Deg2Rad;
-            float x = Radius * Mathf.Cos(rad);
-            float y = Radius * Mathf.Sin(rad);
+            float rad = (210f - (360f / VerticesCount) * (j - 1)) * Mathf.Deg2Rad;
+            float x = KeyRadius * Mathf.Cos(rad);
+            float y = KeyRadius * Mathf.Sin(rad);
+            vertices.Add(new Vector3(x, y, 0));
+            x = HoleRadius * Mathf.Cos(rad);
+            y = HoleRadius * Mathf.Sin(rad);
             vertices.Add(new Vector3(x, y, 0));
         }
 
         //頂点インデクス
         triangles.Add(0);
+        triangles.Add(2);
+        triangles.Add(1);
         triangles.Add(1);
         triangles.Add(2);
+        triangles.Add(3);
 
         var mesh = new Mesh();
         mesh.vertices = vertices.ToArray();
@@ -83,4 +93,84 @@ public class Draw : playMain {
         return mesh;
     }
 
+    private const float JudgePoint = 4.5f;
+    private const float LineWidth = 0.05f;
+    private GameObject JudgeLine;
+    private void CreateJudgeLine()
+    {
+        List<Vector3> vertices = new List<Vector3>();
+        List<int> index = new List<int>();
+        //setting vector
+        for (int i = 0; i < VerticesCount; i++)
+        {
+            float rad = (90f - (360f / VerticesCount) * i) * Mathf.Deg2Rad;
+            for (int j = -1; j <= 1; j = j + 2)
+            {
+                float x = (JudgePoint + LineWidth * j) * Mathf.Cos(rad);
+                float y = (JudgePoint + LineWidth * j) * Mathf.Sin(rad);
+                vertices.Add(new Vector3(x, y, -1));
+            }
+        }
+        //setting index
+        for(int i = 0; i < VerticesCount; i++)
+        {
+            index.Add((i * 2) % vertices.Count);
+            index.Add(((i * 2) + 1) % vertices.Count);
+            index.Add(((i * 2) + 3) % vertices.Count);
+            index.Add((i * 2) % vertices.Count);
+            index.Add(((i * 2) + 3) % vertices.Count);
+            index.Add(((i * 2) + 2) % vertices.Count);
+        }
+        //for (int i = 0; i < vertices.Count; i++) Debug.Log("vertices[" + i + "]:" +vertices[i].x + "," + vertices[i].y + "," + vertices[i].z);
+        //for (int i = 0; i < index.Count; i++) Debug.Log("index[" + i + "]:" + index[i]);
+
+        var mesh = new Mesh();
+        mesh.vertices = vertices.ToArray();
+        mesh.triangles = index.ToArray();
+        JudgeLine = Instantiate(JudgeLinePrehub) as GameObject;
+        JudgeLine.name = "JudgeLine";
+        JudgeLine.GetComponent<MeshFilter>().sharedMesh = mesh;
+    }
+
+    private GameObject LaneLine;
+    private void CreateLaneLine()
+    {
+        List<Vector3> vertices = new List<Vector3>();
+        List<int> index = new List<int>();
+        float RadWidth = 1f;
+        //setting vector
+        for (int i = 0; i < VerticesCount; i++)
+        {
+            for (int j = -1; j <= 1; j = j + 2)
+            {
+                float rad = (90f - (360f / VerticesCount) * i - RadWidth * j) * Mathf.Deg2Rad;
+                float x = KeyRadius * Mathf.Cos(rad);
+                float y = KeyRadius * Mathf.Sin(rad);
+                vertices.Add(new Vector3(x, y, -0.5f));
+                x = HoleRadius * Mathf.Cos(rad);
+                y = HoleRadius * Mathf.Sin(rad);
+                vertices.Add(new Vector3(x, y, 0));
+            }
+        }
+        //setting index
+        for (int i = 0; i < VerticesCount; i++)
+        {
+            int x = i * 4;
+            index.Add(x);
+            index.Add(x + 2);
+            index.Add(x + 1);
+            index.Add(x + 1);
+            index.Add(x + 2);
+            index.Add(x + 3);
+        }
+        //for (int i = 0; i < vertices.Count; i++) Debug.Log("vertices[" + i + "]:" +vertices[i].x + "," + vertices[i].y + "," + vertices[i].z);
+        //for (int i = 0; i < index.Count; i++) Debug.Log("index[" + i + "]:" + index[i]);
+
+        var mesh = new Mesh();
+        mesh.vertices = vertices.ToArray();
+        mesh.triangles = index.ToArray();
+        LaneLine = Instantiate(LaneLinePrehub) as GameObject;
+        LaneLine.name = "LaneLine";
+        LaneLine.GetComponent<MeshFilter>().sharedMesh = mesh;
+    }
 }
--- Assets/Script/playMain.cs
+++ Assets/Script/playMain.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class playMain : MonoBehaviour {
+
     AudioSource music;
-    Draw keyEffect;
+    Draw draw;
     Judge judge;
     bool[] temp = new bool[6];
-
     public KeyCode[] KeyConfig = new KeyCode[6] { KeyCode.V, KeyCode.D, KeyCode.R, KeyCode.U, KeyCode.K, KeyCode.N };
     public float notesDelay = 0.5f;
 
@@ -18,7 +18,7 @@ public class playMain : MonoBehaviour {
         int i;
         for (i = 0; i < 6; i++) temp[i] = false; //キーの初期化
         judge = gameObject.GetComponent<Judge>(); //判定有効化
-        keyEffect = gameObject.GetComponent<Draw>(); //キーエフェクトの有効化
+        draw = gameObject.GetComponent<Draw>(); //描画系の有効化
         music = GameObject.Find("testMusic").GetComponent<AudioSource>(); //音源読み込み
         //準備ができたらコルーチンスタート
         StartCoroutine("playMusicGame");
@@ -39,29 +39,25 @@ public class playMain : MonoBehaviour {
                 //Debug.Log(playTime);
             }
 
-            int i;
-            for (i = 0; i < 6; i++)
+            
+            for (int i = 0; i < 6; i++)
             {
                 if (Input.GetKey(KeyConfig[i]) != temp[i])
                 {
                     //Debug.Log(i);
                     if (temp[i])
                     {
-                        keyEffect.TurnOff(i);
+                        draw.TurnOff(i);
                         temp[i] = false;
                     }
                     else
                     {
-                        keyEffect.TurnOn(i, judge.OnKey(i, playTime));
+                        draw.TurnOn(i, judge.OnKey(i, playTime));
                         temp[i] = true;
                     }
                 }
             }
-            if (Input.GetKeyDown(KeyCode.Escape))
-            {
-                Debug.Log("Coroutine end.");
-                yield break;
-            }
+
             yield return new WaitForSeconds(0.03f);
         }
     }
