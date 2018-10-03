9f8c3209fd74ff0037760580cf102f70bc0c1c2d
Missionary <red731_ninap@outlook.jp>
2018-10-03 10:14:40 +0900

    仮コーディング完了

--- Assets/Script/MusicListLoader.cs
+++ Assets/Script/MusicListLoader.cs
@@ -8,16 +8,55 @@ public class MusicListLoader : MonoBehaviour {
     [Serializable]
     public class MusicList
     {
+        public Music[] music;
+    }
 
-    } 
+    [Serializable]
+    public class Music
+    {
+        public string title;
+        public string artist;
+        public string scoreMake;
+        public Difficulty difficulty;
+        public float bpm;
+        public int HiScore;
+        public string FileName;
+        public string JacketFileName;
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    [Serializable]
+    public class Difficulty
+    {
+        public LevelInfo easy;
+        public LevelInfo normal;
+        public LevelInfo hard;
+    }
+
+    [Serializable]
+    public class LevelInfo
+    {
+        public int level;
+        public int AllTarget;
+    }
+
+
+    MusicList musicList;
+
+    // Use this for initialization
+    public MusicList GetMusicList () {
+        StartCoroutine("LoadList");
+        return musicList;
+    }
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    IEnumerator LoadList()
+    {
+        using (WWW www = new WWW("file:///" + Application.dataPath + "/Resouces/MusicList.json"))
+        {
+            yield return www;
+            yield return musicList = JsonUtility.FromJson<MusicList>(www.text);
+
+        }
+    }
+
+
 }
