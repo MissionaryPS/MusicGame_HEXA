e58b552382d29a2fcfef39f4bbc3ba2c8db2ff07
MissionaryPS <red731_NinaP@outlook.jp>
2018-12-01 10:58:49 +0900

    fixing conflict 2

--- Assets/Script/MainRoot.cs
+++ Assets/Script/MainRoot.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 using System;
 
 
-public class MainRoot : MonoBehaviour {
+public class MainRoot : MonoBehaviour
+{
 
     public KeyCode[] KeyConfig = new KeyCode[6] { KeyCode.V, KeyCode.D, KeyCode.R, KeyCode.U, KeyCode.K, KeyCode.N };
     public bool[] isOnKey = new bool[6];
@@ -12,11 +13,43 @@ public class MainRoot : MonoBehaviour {
 
     public bool LoadData = false;
 
-    // Use this for initialization
-    void Start () {
-        
-	}
-	
-	// Update is called once per frame
-	
+
+    //六角形系統
+    [SerializeField]
+    public GameObject HexBase;
+
+    public GameObject CreateHexagon(Vector3 center, float radius)
+    {
+        List<int> index = new List<int>();
+        for (int i = 1; i <= 6; i++)
+        {
+            index.Add(0);
+            index.Add(i);
+            index.Add((i + 1 <= 6) ? i + 1 : 1);
+        }
+        var mesh = new Mesh();
+        mesh.vertices = CalcVertices(center, radius).ToArray();
+        mesh.triangles = index.ToArray();
+        GameObject Hexagon = Instantiate(HexBase) as GameObject;
+        Hexagon.GetComponent<MeshFilter>().sharedMesh = mesh;
+
+        return Hexagon;
+    }
+
+    public List<Vector3> CalcVertices(Vector3 center, float radius)
+    {
+        List<Vector3> vertices = new List<Vector3>();
+        vertices.Add(center);
+        for (int i = 0; i < 6; i++)
+        {
+            float rad = (90f - 60f * i) * Mathf.Deg2Rad;
+            float x = radius * Mathf.Cos(rad);
+            float y = radius * Mathf.Sin(rad);
+            vertices.Add(center + new Vector3(x, y, 0));
+        }
+        return vertices;
+    }
+
+    // Update is called once per frame
+
 }
