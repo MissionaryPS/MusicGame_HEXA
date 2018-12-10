a95f190e34120c03a5fb346b79a4ce30170556b2
MissionaryPS <red731_NinaP@outlook.jp>
2018-12-10 16:30:24 +0900

    タイトル・難易度表示

--- Assets/Script/Select/ButtonManager.cs
+++ Assets/Script/Select/ButtonManager.cs
@@ -4,10 +4,59 @@ using UnityEngine;
 
 public class ButtonManager : SelectMain {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
+    [SerializeField]
+    float radius;
+
+    public MusicList musicList;
+    public List<Material> ButtonSurface = new List<Material>();
+    int devide = 15;
+    int difficulty = 0;
+    //private ButtonMaterial Button;
+    GameObject[] MusicButton;
+
+
+    public void SetUp(int center, int difficulty, MusicList musicList) {
+      
+        float CircleRadius = radius * Mathf.Cos((360f / devide) * Mathf.Deg2Rad) / Mathf.Tan((360f / devide / 2) * Mathf.Deg2Rad);
+        
+        //music button
+        MusicButton = new GameObject[musicList.music.Length + 2];
+        for (int i = 0; i < musicList.music.Length + 2; i++)
+        {
+            MusicButton[i] = Instantiate(HexBase) as GameObject;
+            MusicButton[i].GetComponent<MusicButton>().SetUpButton(i, center, devide, radius, CircleRadius, difficulty);
+        }
+
+        //menubutton
+        float CRadius = radius * Mathf.Sin(60 * Mathf.Deg2Rad) * 2;
+        GameObject[] MenuButton = new GameObject[4];
+        for (int i = 0; i < 6; i++)
+        {
+            int j = 0;
+            if (i != 0 && i != 3)
+            {
+                float rad = (60f * i) * Mathf.Deg2Rad;
+                float cx = CRadius * Mathf.Cos(rad);
+                float cy = CRadius * Mathf.Sin(rad);
+
+                MenuButton[j++] = CreateHexagon(new Vector3(cx, cy, -CircleRadius), radius - 0.1f);
+                //MenuButton[j].name = "MenuButton";
+            }
+        }
+    }
+
+    public void Replace(int center)
+    {
+        foreach (GameObject Button in MusicButton)
+            Button.GetComponent<MusicButton>().ReDrawButton(center);
+
+    }
+
+    public void ChangeDifficulty(int difficulty) {
+        foreach (GameObject Button in MusicButton)
+            Button.GetComponent<MusicButton>().ChangeDifficulty(difficulty);
+    }
+
+
 
 }
---
+++ Assets/Script/Select/SelectManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectManager : SelectMain {
+
+    public MusicList musicList;
+    private CanvasManager canvas;
+    private ButtonManager buttonManager;
+    int center;
+    int difficulty;
+
+    // Use this for initialization
+    void Start() {
+
+        canvas = gameObject.GetComponent<CanvasManager>();
+        buttonManager = gameObject.GetComponent<ButtonManager>();
+        center = 1;
+        difficulty = 0;
+        StartCoroutine("enumerator");
+    }
+    IEnumerator enumerator (){
+        yield return StartCoroutine("LoadList");
+        Debug.Log(musicList.music.Length + "musics load");
+        canvas.SetUp(center, difficulty, musicList);
+        buttonManager.SetUp(center, difficulty, musicList);
+        while (true)
+        {
+            if (!(isOnKey[1]) && Input.GetKey(KeyConfig[1]))
+            {
+                center = center == 0 ? musicList.music.Length + 1 : center - 1;
+                canvas.ChangeMusic(center, difficulty);
+                buttonManager.Replace(center);
+            }
+            if (!(isOnKey[4]) && Input.GetKey(KeyConfig[4]))
+            {
+                center = center == musicList.music.Length + 1 ? 0 : center + 1;
+                canvas.ChangeMusic(center, difficulty);
+                buttonManager.Replace(center);
+            }
+            if (!(isOnKey[3]) && Input.GetKey(KeyConfig[3]))
+            {
+                difficulty = (difficulty + 1) % 3;
+                canvas.ChangeMusic(center, difficulty);
+                buttonManager.ChangeDifficulty(difficulty);
+            }
+            //if (!(isOnKey[7]) && Input.GetKey(KeyConfig[7])) yield break;
+
+            for (int i = 0; i < KeyConfig.Length; i++) isOnKey[i] = Input.GetKey(KeyConfig[i]);
+            yield return new WaitForSeconds(fps);
+        }
+    }
+
+    IEnumerator LoadList()
+    {
+        using (WWW www = new WWW("file:///" + Application.dataPath + "/Resources/MusicList.json"))
+        {
+            yield return www;
+            Debug.Log(www.url);
+            Debug.Log(www.isDone);
+            Debug.Log(www.text);
+            yield return musicList = JsonUtility.FromJson<MusicList>(www.text);
+        }
+        yield break;
+    }
+
+    private MusicList SortList(MusicList musicList, string type, bool reverse)
+    {
+        //実装終わってない
+        MusicList retList = musicList;
+
+        return retList;
+    }
+
+}
