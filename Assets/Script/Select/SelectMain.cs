5f1aaca98d76f3b90be4b38c21591285830a652f
MissionaryPS <red731_NinaP@outlook.jp>
2018-12-10 18:31:09 +0900

    Revert "pull"
    This reverts commit c0af5bb64e953f1b762a78c8af82dfcd8a1956bb, reversing
    changes made to a95f190e34120c03a5fb346b79a4ce30170556b2.

--- Assets/Script/Select/SelectMain.cs
+++ Assets/Script/Select/SelectMain.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 using System;
 
 
-public class SelectMain : MainRoot
-{
+public class SelectMain : MainRoot {
 
     [Serializable]
     public class MusicList
@@ -37,117 +36,6 @@ public class SelectMain : MainRoot
         public int AllTarget;
     }
 
-<<<<<<< HEAD
-=======
-    [SerializeField]
-    float radius;
-
-    public MusicList musicList;
-    public List<Material> ButtonSurface = new List<Material>();
-    [SerializeField]
-    public Material nomusic;
-    [SerializeField]
-    public Material existmusic;
-
-    int devide = 15;
-    int difficulty = 0;
-    //private ButtonMaterial Button;
-
-    IEnumerator Start()
-    {
-        yield return StartCoroutine("LoadList");
-        //Button = gameObject.GetComponent<ButtonMaterial>();
-        //foreach (Music music in musicList.music) Debug.Log(music.title);
-        Debug.Log(musicList.music.Length + "musics load");
-
-        float CircleRadius = radius * Mathf.Cos((360f / devide) * Mathf.Deg2Rad) / Mathf.Tan((360f / devide / 2) * Mathf.Deg2Rad);
-
-
-        //music button
-        int center = 1;
-        GameObject[] MusicButton = new GameObject[musicList.music.Length + 2];
-        for (int i = 0; i < musicList.music.Length + 2; i++)
-        {
-            MusicButton[i] = Instantiate(HexBase) as GameObject;
-
-            MusicButton[i].GetComponent<MusicButton>().SetUpButton(i, center, devide, radius, CircleRadius,difficulty);
-        }
-
-        //menubutton
-        float CRadius = radius * Mathf.Sin(60 * Mathf.Deg2Rad) * 2;
-        GameObject[] MenuButton = new GameObject[4];
-        for (int i = 0; i < 6; i++)
-        {
-            int j = 0;
-            if (i != 0 && i != 3)
-            {
-                float rad = (60f * i) * Mathf.Deg2Rad;
-                float cx = CRadius * Mathf.Cos(rad);
-                float cy = CRadius * Mathf.Sin(rad);
-                
-                MenuButton[j++] = CreateHexagon(new Vector3(cx, cy, -CircleRadius), radius-0.1f);
-                //MenuButton[j].name = "MenuButton";
-            }
-        }
-        
-        
-
-        //選択処理開始
-        while (true)
-        {
-            if (!(isOnKey[1]) && Input.GetKey(KeyConfig[1]))
-            {
-                center = center == 0 ?  musicList.music.Length + 1 : center - 1;
-                foreach (GameObject Button in MusicButton)
-                    Button.GetComponent<MusicButton>().ReDrawButton(center);
-
-            }
-            if (!(isOnKey[4]) && Input.GetKey(KeyConfig[4]))
-            {
-                center = center == musicList.music.Length + 1 ? 0 : center + 1;
-                foreach (GameObject Button in MusicButton)
-                    Button.GetComponent<MusicButton>().ReDrawButton(center);
-            }
-            if (!(isOnKey[3]) && Input.GetKey(KeyConfig[3]))
-            {
-                difficulty = (difficulty + 1) % 3;
-                foreach (GameObject Button in MusicButton)
-                    Button.GetComponent<MusicButton>().ChangeDifficulty(difficulty);
-            }
-            //if (!(isOnKey[7]) && Input.GetKey(KeyConfig[7])) yield break;
-
-            for (int i = 0; i < KeyConfig.Length; i++) isOnKey[i] = Input.GetKey(KeyConfig[i]);
-            yield return new WaitForSeconds(fps);
-        }
-
-    }
-
-    //読み込み系統
-    IEnumerator LoadList()
-    {
-        using (WWW www = new WWW("file:///" + Application.dataPath + "/Resources/MusicList.json"))
-        {
-            yield return www;
-            Debug.Log(www.url);
-            Debug.Log(www.isDone);
-            Debug.Log(www.text);
-            yield return musicList = JsonUtility.FromJson<MusicList>(www.text);
-        }
-        yield break;
-    }
-
-    private MusicList SortList(MusicList musicList, string type, bool reverse)
-    {
-        MusicList retList = musicList;
-
-        return retList;
-    }
-
-
-
-
-
->>>>>>> 167a9a8cd95d429763f9448fb6914b3f35931dd7
 
 
 }
