c8883a1a27c45b0e6e06c25dad65e05f4235727b
MissionaryPS <red731_NinaP@outlook.jp>
2019-01-29 13:24:47 +0900

    press any buttonの文字が明滅するアレの実装

---
+++ Assets/Script/TextBlinking.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextBlinking : MainRoot {
+
+    Text text;
+    float change = 0.025f;
+
+	// Use this for initialization
+	void Start () {
+        text = gameObject.GetComponent<Text>();
+        StartCoroutine("Blink");
+    }
+
+    IEnumerator Blink()
+    {
+        while (true)
+        {
+            Color now = text.color;
+            Debug.Log("a:" + now.a);
+            if (text.color.a > 1.0f) text.color = new Color(now.r, now.g, now.b, 0.0f);
+            else text.color = new Color(now.r, now.g, now.b, now.a + change);
+            yield return new WaitForSeconds(fps);
+        }
+    }
+	
+}
